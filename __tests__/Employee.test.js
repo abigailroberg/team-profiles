783a26257323f1edb2e69d88236b7d099eaa1f21
Abby Roberg <abbyroberg@gmail.com>
2021-09-04 13:47:49 -0500

    get employee info functions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,10 +9,28 @@ test('creates a new employee object', () => {
     expect(employee.email).toBe('email')
 })
 
-test("gets employee info as an object", () => {
+test('gets employee info as an object', () => {
     const employee = new Employee('name', 'id', 'email')
   
     expect(employee.getRole()).toHaveProperty('name');
     expect(employee.getRole()).toHaveProperty('id');
     expect(employee.getRole()).toHaveProperty('email')
+})
+
+test('gets employee name', () => {
+    const employee = new Employee('name', 'id', 'email')
+
+    expect(employee.getName()).toBe('name')
+})
+
+test('gets employee id', () => {
+    const employee = new Employee('name', 'id', 'email')
+
+    expect(employee.getId()).toBe('id')
+})
+
+test('gets employee email', () => {
+    const employee = new Employee('name', 'id', 'email')
+
+    expect(employee.getEmail()).toBe('email')
 })
